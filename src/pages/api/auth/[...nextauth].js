cab95a4966ab0954f5e7265af4409812a8dc17a6
J.M. Cléry <jmclery@gmail.com>
2021-05-21 23:04:02 +0200

    Adds GitHub and Facebook as providers for llogin

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,14 @@ export default NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
+        Providers.GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        Providers.Facebook({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+        }),
         // ...add more providers here
     ],
 });
